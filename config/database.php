dd34828a6c56d48109acabc163edd659bf6c0d6e
thallisphp <thallisphp@gmail.com>
2016-04-30 15:41:11 -0300

    Mais ajustes para TravisCI

--- config/database.php
+++ config/database.php
@@ -48,7 +48,13 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => storage_path('database.sqlite'),
+            'prefix' => '',
+        ],
+
+        'memory' => [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
             'prefix' => '',
         ],
 
